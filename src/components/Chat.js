0dc485457d5e92421afb22f03b79f16d1a4095ce
shaikwasef <wasefshaik@gmail.com>
2021-02-25 19:58:00 +0530

    1 file modified

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,27 +1,54 @@
-import React from 'react';
-import styled from 'styled-components';
-import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
+import React from "react";
+import styled from "styled-components";
+import InfoOutlinedIcon from "@material-ui/icons/InfoOutlined";
+import StarBorderIcon from '@material-ui/icons/StarBorder';
 
 function Chat() {
   return (
     <Container>
-      <NameContainer>
-       <Name>  #Clever </Name>
-       <Details> Details <InfoOutlinedIcon/> </Details>
-      </NameContainer>
+      <Description>
+        <NameContainer>
+          <Name>#clever</Name>
+          <StarBorderIcon    style = {{fontSize : "medium"}}/>
+        </NameContainer>
+        <DetailsContainer>
+          <Details>Details</Details>
+          <InfoOutlinedIcon  />
+        </DetailsContainer>
+      </Description>
     </Container>
   );
 }
 
 export default Chat;
 
-const Container = styled.div`
-display : flex;
+const Container = styled.div``;
+
+const Description = styled.div`
+display : flex ;
+align-items : center ;
+margin : 10px 10px 0px 10px ;
 ` ;
+
 const NameContainer = styled.div`
 display : flex ;
-align - items : center ;
-justify-content : space-between ; 
-` ;
-const Name = styled.div`` ;
-const Details = styled.div`` ;
+align-items : center ;
+font-weight: bolder;
+`;
+
+const Name = styled.div`
+  display : flex ;
+  flex-wrap : wrap ;
+  align-items : center ;
+  font-size : 20px ;
+`;
+
+const Details = styled.div`
+`;
+
+const DetailsContainer = styled.div`
+color : #C1C1C1;
+display : flex ;
+align-items : center ;
+`;
+
