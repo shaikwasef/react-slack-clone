1cc4ed49b204ac70793577e509465b875ceea8b2
shaikwasef <wasefshaik@gmail.com>
2021-02-25 12:43:32 +0530

    1 file modified, 3 added, 2 deleted

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React from "react";
-import "./style.css";
-import {BrowserRouter as Router , Route , Switch} from 'react-router-dom';
-import Chat from "./components/Chat"
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Chat from "./components/Chat";
+import Login from "./components/Login.js";
 
 export default function App() {
   return (
     <div className="App">
       <Router>
-        <Switch>
-          <Route path = "/room">
+       <Container>
+       <header />
+          <Switch>
+            <Route path="/room">
               <Chat />
             </Route>
-            <Route path = "/">
-              Login Page
+            <Route path="/">
+              <Login />
             </Route>
           </Switch>
+        </Container>
       </Router>
     </div>
   );
--- src/components/Chat.js
+++ src/components/Chat.js
@@ -3,6 +3,7 @@ import React from 'react'
 function Chat() {
   return (
     <div>
+    Chat Page
     </div>
   );
 }
