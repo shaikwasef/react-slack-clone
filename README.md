43420b322133d56dc346777188d2bba5e19c9397
shaikwasef <wasefshaik@gmail.com>
2021-02-25 12:29:26 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-xn9jv2
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-xn9jv2)
---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./style.css";
+import {BrowserRouter as Router , Route , Switch} from 'react-router-dom';
+
+export default function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route path = "/room">
+            Chat Page
+            </Route>
+            <Route path = "/">
+              Login Page
+            </Route>
+          </Switch>
+      </Router>
+    </div>
+  );
+}
