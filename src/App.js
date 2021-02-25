bdec1cde59a5b5b27b5fd66c539f3a5a614075d2
shaikwasef <wasefshaik@gmail.com>
2021-02-25 12:30:50 +0530

    1 file modified, 1 added, 1 deleted

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./style.css";
 import {BrowserRouter as Router , Route , Switch} from 'react-router-dom';
+import Chat from "./components/Chat"
 
 export default function App() {
   return (
